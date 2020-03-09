48c16d1a212b1111b5aabf64f3f69ee2458c69ba
James Woolfenden <james.woolfenden@slalom.com>
2020-03-09 22:46:37 +0000

    sample

---
+++ variables.tf
@@ -0,0 +1,25 @@
+
+variable "efs" {
+  description = "To create a shared file share"
+  type        = map
+
+  default = {
+    creation_token                  = "my-product"
+    transition_to_ia                = "AFTER_30_DAYS"
+    kms_key_id                      = ""
+    encrypted                       = true
+    performance_mode                = "generalPurpose"
+    provisioned_throughput_in_mibps = 0
+    throughput_mode                 = "bursting"
+  }
+}
+
+variable "mounts" {
+  type        = list
+  description = "Subnets to create mount points for"
+  default = [{
+    subnet_id       = ""
+    ip_address      = ""
+    security_groups = []
+  }]
+}
